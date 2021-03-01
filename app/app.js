0dd304ed6c631d1b4aa4b0c8c7b93d0b99dba7de
argi47 <argi47@msn.com>
2021-03-01 10:22:37 +0100

    Server connection

--- app/app.js
+++ app/app.js
@@ -6,4 +6,18 @@ const config = require("./config");
 function bootstraping() {
   const app = express();
   const server = app.listen(config.server.port);
+
+  server.on("error", onError);
+  server.on("listening", function () {
+    console.info(
+      `Server is running on http://${config.server.host}:${config.server.port}`
+    );
+    // loader(app);
+  });
 }
+
+function onError() {
+  console.log("Error");
+}
+
+bootstraping();
--- app/config.js
+++ app/config.js
@@ -4,3 +4,5 @@ const config = {
     port: process.env.PORT || 5500,
   },
 };
+
+module.exports = config;
