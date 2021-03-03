a3dbde8c8e0b7f2b18ecfc99e67aa9d91856231f
argi47 <argi47@msn.com>
2021-03-03 15:48:05 +0100

    candidates get and post ready

--- app/models/Candidate.js
+++ app/models/Candidate.js
@@ -1,6 +1,23 @@
 const mongoose = require("mongoose");
 
-const candidateSchema = new mongoose.Schema();
+const candidateSchema = new mongoose.Schema({
+  name: String,
+  surname: String,
+  email: String,
+  avatar: String,
+  age: Number,
+  rol: String,
+  phone: String,
+  city: String,
+  keyWords: {
+    creativity: Boolean,
+    uxui: Boolean,
+    sketch: Boolean,
+    projects: Boolean,
+    empathy: Boolean,
+    html: Boolean,
+  },
+});
 
 const Candidate = mongoose.model("Candidate", candidateSchema);
 
--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,22 @@
 const express = require("express");
 
 const offerController = require("./controllers/offerController");
+const candidatesController = require("./controllers/candidateController");
 
 const router = express.Router();
 
-// ---------- OFFER ROUTES ----------
+// ---------- OFFERS ROUTES ----------
 
 router
   .route("/offers")
   .get(offerController.listOffers)
   .post(offerController.createOffer);
+
+// ---------- CANDIDATES ROUTES ----------
+
+router
+  .route("/candidates")
+  .get(candidatesController.listCandidates)
+  .post(candidatesController.createCandidate);
 
 module.exports = router;
