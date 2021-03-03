d734ac7fdbfbacbb2082d32f88ceedb4f81fee1a
i-am-fanny <fannyfranchy@hotmail.com>
2021-03-03 13:30:05 +0100

    bcrypt compare login

--- app/routes.js
+++ app/routes.js
@@ -4,10 +4,33 @@ const offerController = require("./controllers/offerController");
 const userController = require('./controllers/userController');
 
 const User = require('./models/User');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
-// ---------- USERS ROUTES ----------
+// ---------- LOGIN ROUTES ----------
+
+router.route('/login')
+    .post(async function(req, res, next) {
+      try{
+        const foundUser = await User.findOne({email: req.body.email});
+        if (foundUser === null) {
+          res.status(200).json({ error: 'User not found' });
+        }
+        else if (await bcrypt.compare(req.body.password, foundUser.password)) {
+          res.status(200).json({ error: false, logged: foundUser });
+        }
+        else {
+          res.status(200).json({ error: 'Password incorrect' });
+        }
+      }
+      catch(err) {
+        next(err);
+      }
+    });
+
+
+// ---------- USER ROUTES ----------
 
 router.route('/user')
     .post(userController.createUser)
