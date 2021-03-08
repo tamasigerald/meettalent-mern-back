302fc4737fcaa2b09bc26b02b19940753feaa9e4
i-am-fanny <fannyfranchy@hotmail.com>
2021-03-08 11:23:29 +0100

    login ok

--- app/loaders/passportLoader.js
+++ app/loaders/passportLoader.js
@@ -1,54 +1,12 @@
 const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+const localStrategies = require('./strategies/localStrategyLoader');
 
-const crudUser = require("../business/crudUser");
+
 
 function passportLoader(app) {
-    passport.use(
-        "register-local",
-        new LocalStrategy(
-            {
-                usernameField: "email",
-                passReqToCallback: true,
-            },
-            function (req, email, password, done) {
-                crudUser
-                    .createUser(req.body.email, req.body.password)
-                    .then(function (newUser) {
-                        done(null, newUser);
-                    })
-                    .catch(function (err) {
-                        done(err);
-                    });
-            }
-        )
-    );
+    passport.use("register-local", localStrategies.registerStrategy);
 
-    passport.use('login-local', new LocalStrategy(
-        {
-            usernameField: 'email',
-            passReqToCallback: true,
-        },
-        function (req, email, password, done) {
-            crudUser.getUserByEmail(req.body.email)
-                .then(function (userFound) {
-                    if (userFound) {
-                        if (userFound.checkPassword(req.body.password)) {
-                            return done(null, userFound);
-                        }
-                        else {
-                            done(null, false);
-                        }
-                    }
-                    else {
-                        done(null, false);
-                    }
-                })
-                .catch(function (err) {
-                    done(err)
-                });
-        }
-    ));
+    passport.use('login-local', localStrategies.loginStrategy);
 
     app.use(passport.initialize());
 }
